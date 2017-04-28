f6fe188f52b3120da89f23a1211ae8cd08bd1ad7
Scott Kriss <skriss@deloitte.com>
2017-04-28 15:21:08 -0700

    added clear button functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString:String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func didPressPlus(_ sender: Any) {
+    }
+    @IBAction func didPressSubtract(_ sender: Any) {
+    }
+    @IBAction func didPressEquals(_ sender: Any) {
+    }
+    @IBAction func didPressClear(_ sender: Any) {
+        
+        labelString = "0"
+        currentMode = .not_set
+        savedNum = 0
+        lastButtonWasMode = false
+        label.text = "0"
+        
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        
+        let stringValue:String? = sender.titleLabel?.text
+        labelString = labelString.appending(stringValue!)
+        updateText()
+        
+    }
+    
+    func updateText(){
+        guard let labelInt:Int = Int(labelString) else {
+            return
+        }
+        label.text = "\(labelInt)"
+    }
+    
+    func changeMode(newMode:modes){
+        
+        
+    }
+    
+    
+    
 }
 
